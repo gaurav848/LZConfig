4868035c4d052ac7dbc5821c663e649da81922d1
david wallwin <dawa@lubrizol.com>
2016-01-20 14:29:04 +0000

    Bug fix

--- LZConfig.Web/app/components/application/applicationController.ts
+++ LZConfig.Web/app/components/application/applicationController.ts
@@ -14,11 +14,14 @@
 
         onClickTab(tab) {
             this.currentTab = tab.url;
-            console.log(tab);
+        }
+
+        setActiveTab(tabUrl: string) {
+            this.currentTab = tabUrl;
+            console.log("setActiveTab:" + tabUrl);
         }
 
         isActiveTab(tabUrl: string) {
-            console.log(tabUrl);
             return tabUrl === this.currentTab;
         }
 
@@ -27,10 +30,9 @@
             private dataAccessService: lzconfig.services.DataAccessService,
             private $location: ng.ILocationService) {
 
-            this.currentTab = "app/components/application/applicationDetailView.html";
             this.title = "Application"
             this.tabs = [{ title: "Application", url: "app/components/application/applicationDetailView.html" }, { title: "Connection Strings", url: "app/components/connections/applicationConnectionsView.html" }, { title: "Application Variables", url: "app/components/variables/applicationVariablesView.html" }];
-            this.currentTab = "applicationDetailView.html";
+            this.currentTab = "app/components/application/applicationDetailView.html";
 
             var applicationResource = dataAccessService.getApplicationResource();
 
